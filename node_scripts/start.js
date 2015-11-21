29e629549e4768d73d96d65975a3a69aa8bede28
skullmasher <skullmasher@heartlessgaming.com>
2015-11-21 20:03:23 +0100

    :metal: adding browser-sync and Error message on sass error

--- node_scripts/start.js
+++ node_scripts/start.js
@@ -1,45 +1,67 @@
 var fs = require('fs');
 var chalk = require('chalk');
 var sass = require('node-sass');
-var browserSync = require("browser-sync");
+var bs = require("browser-sync").create();
 
-console.log( chalk.red('  #####   ') );
-console.log( chalk.red(' #######  ') );
-console.log( chalk.red('#  ###  # ') + chalk.grey(' The mighty Skull is starting your project.' ) );
-console.log( chalk.red('#   #   # ') );
-console.log( chalk.red('######### ') + chalk.grey(' Happy coding !' ) );
-console.log( chalk.red(' ### ###  ') );
-console.log( chalk.red('  #####   ') );
-console.log( chalk.red('  # # #   ') + chalk.grey(' Play more, care less, be an heartless' ));
+// console.log for 1337 h4X0r
+var log = console.log.bind(console);
 
-console.log( '' )
-console.log( chalk.grey('Compiling sass files...' ) )
-sass.render({
-  file: 'sass/style.scss',
-  outputStyle: 'expanded',
-  outFile: 'styles/style.css',
-  sourceMap: true
-}, function(error, result) { // node-style callback from v3.0.0 onwards
-  if (error) {
-    console.log(error.status); // used to be "code" in v2x and below
-    console.log(error.column);
-    console.log(error.message);
-    console.log(error.line);
-  } else {
-    fs.writeFile('styles/style.css', result.css, function(err){
-      if(!err){
-        console.log(chalk.grey('CSS file generated.'));
-      } else {
-        console.log( err );
-      }
-    });
-    fs.writeFile('styles/style.map.css', result.map, function(err){
-      if(!err){
-        console.log(chalk.grey('CSS map file generated.'));
-      } else {
-        console.log( err );
-      }
-    });
+// Greeting Message
+log( chalk.red('  #####   ') );
+log( chalk.red(' #######  ') );
+log( chalk.red('#  ###  # ') + chalk.grey(' The mighty Skull is starting your project.' ) );
+log( chalk.red('#   #   # ') );
+log( chalk.red('######### ') + chalk.grey(' Happy coding !' ) );
+log( chalk.red(' ### ###  ') );
+log( chalk.red('  #####   ') );
+log( chalk.red('  # # #   ') + chalk.grey(' Play more, care less, be an heartless' ));
 
-  }
+// Reload all browser on html change
+bs.watch("*.html").on("change", bs.reload);
+
+// Specific compilation for sass file
+bs.watch("sass/**.scss", function (event, file) {
+    if (event === "change") {
+
+        sass.render({
+          file: 'sass/style.scss',
+          outputStyle: 'expanded',
+          outFile: 'styles/style.css',
+          sourceMap: true
+        }, function(error, result) {
+          if (error) {
+            // Pretty Debug Message on sass error
+            log( chalk.red('[SASS ERROR] ' ) + error.file )
+            log( chalk.red('[SASS ERROR] ' ) + 'On line ' + chalk.red(error.line) + ' at column ' + chalk.red(error.column) );
+            log( chalk.red('[SASS ERROR] ' ) + error.message );
+
+          } else {
+
+            // Creating css files
+            fs.writeFile('styles/style.css', result.css, function(err){
+              if(!err){
+
+                // Creating css map file
+                fs.writeFile('styles/style.map.css', result.map, function(err) {
+                  if(!err){
+
+                  } else {
+                    log( err );
+                  }
+                });
+              } else {
+                log( err );
+              }
+            });
+            // Injecting the CSS change in BrowserSync
+            bs.reload('styles/style.css');
+          }
+        });
+
+    }
+});
+
+// Now init the Browsersync server
+bs.init({
+    server: "./"
 });
