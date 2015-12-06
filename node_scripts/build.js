7db1e89b7af28678ba1dc2a4107988cf091fcc6e
skullmasher <skullmasher@heartlessgaming.com>
2015-12-06 17:05:29 +0100

    :metal: prmise based clean dist folder method

--- node_scripts/build.js
+++ node_scripts/build.js
@@ -1,17 +1,22 @@
-var fs = require('fs');
-var chalk = require('chalk');
-var dateFormat = require('dateformat');
-var rimraf = require('rimraf');
-var mkdirp = require('mkdirp');
-var sass = require('node-sass');
+var fs          = require('fs');
+var Promise     = require('bluebird');
+var chalk       = require('chalk');
+var dateFormat  = require('dateformat');
+var sass        = require('node-sass');
+var compressor  = require('node-minify');
+var rimraf      = require('rimraf-promise');
 
-
+// http://bluebirdjs.com/docs/api/promisification.html
+var mkdirp = Promise.promisifyAll(require("mkdirp"));
 
 // console.log for 1337 h4X0r
 var log = console.log.bind(console);
 
 // ISO date format to use for debuging
 var now = new Date();
+
+// Build Folder Name
+var buildFolderName = 'dist';
 
 // Greeting Message
 log( chalk.red('  #####   ') );
@@ -23,3 +28,20 @@ log( chalk.red(' ### ###  ') );
 log( chalk.red('  #####   ') );
 log( chalk.red('  # # #   ') + chalk.grey(' Play more, care less, be an heartless' ));
 
+var cleanDistFolder = function( folderPath ) {
+  return rimraf( folderPath )
+    .then( function(){
+      return mkdirp.mkdirpAsync( folderPath );
+    })
+    .then(function(res){
+      return folderPath + ' folder cleaned';
+    })
+    .catch( console.error );
+};
+
+
+
+cleanDistFolder( buildFolderName )
+  .then(function(res){
+    log(res);
+  });
