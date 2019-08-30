9fd91bd846ae22f8e1e420bb6b8eff3ddcca8087
Skullmasher <skullm4sher@gmail.com>
2019-08-30 21:24:42 +0200

    :skull: Adding actual tsquote

--- components/Quotes.vue
+++ components/Quotes.vue
@@ -8,20 +8,19 @@ export default {
     interval: 300000, // 1000*60*5 That's 5minutes !
     quote: 'We will find a way',
     quotes: [
-      'Tengo harambe !',
-      'The measure of inteligence mimimi - probably someone important',
-      'Spain is France, they just swap cheese with jamon',
-      'Time for Trenza !',
-      "Toulouse c'est le pays des chocolatines !",
-      'We will find a way',
-      'Spain is France, they just swap mantequilla with aceite',
-      'jamon != jabon',
-      'Quiero des mimos porfa',
-      'mimimi m!ss you',
-      '5 minutes more !',
-      "I don't want to set the world on fire",
-      'Until the mountains crumbles to sea',
-      'Future awaits !'
+      `Bon c'est une bonne addition à mon dossier "grossisant" ! - GuiltysparK`,
+      `Oh ah pourquoi ya du sang partout ? ah bah il est mort le vendeur de suschi. - Eljam`,
+      `Quest-ce qu'on est sérée , au fond de ce markus ! - Don`,
+      `T'a cas coller 1 milliard de fromton sous ta baraque tu vera si l'odeur remonte pas !! - Don`,
+      `Pour la peine j'ai coller mon coude dans ton mamie nova - Don`,
+      `Ils ont un camouflage naturel, comme les noirs la nuit - GuiltysparK`,
+      `Non t'est pas noir, tes juste gris avec un peu de marron - GuiltysparK`,
+      `Tu veux un cornichon pour faire passer l'amertume ? - Lan Aout 2013`,
+      `L'eau c'est impermeable - Mugetsu`,
+      `Tu va te retrouver comme le culmasher, avec un serveur sur les bras et des trou du cul dessus - Skullmasher`,
+      `Ça fait tellement longtemps qu'on part a volo que ça m'etonne qu'on soit pas deja arrivé ! - Guiltyspark`,
+      `On va y jouer regulierement souvent. - Skullmasher`,
+      `Ah mais on peut jouer a plusieurs en MMO ?? - Skullmasher`
     ]
   }),
   mounted() {
