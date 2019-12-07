f6682b8d955f74528e48e1d8bceb83920d10caeb
Skullmasher <skullm4sher@gmail.com>
2019-12-08 00:44:36 +0100

    :skull: Add all ARK info

--- pages/ark-information-serveur.vue
+++ pages/ark-information-serveur.vue
@@ -29,9 +29,10 @@
       </p>
       <h2>Configuration du serveur</h2>
       <ul>
+        <li>PVE activé</li>
         <li>
           Pas d'attente de 24h pour changer ses statistiques après consomation
-          d'un mind wipe tonic.
+          d'un mind wipe tonic
         </li>
         <li>Vitesse d'éclosion des oeufs x10</li>
         <li>Maturation des bébé dino x20</li>
@@ -39,7 +40,39 @@
           Qualité des loots dans les rayons 2 (valeur original 1, valeur maximal
           5)
         </li>
-        <li>Les dino capturer ont deux fois moins faim que la normal.</li>
+        <li>
+          Les dinos capturer ont deux fois moins faim que la normal
+        </li>
+        <li>
+          Les dinos volants sont autorisé à transporter les joueur du serveur
+        </li>
+        <li>La caméra 3ème personnes est autorisé</li>
+        <li>Notifications lorsqu'un joueur ce connecte ou ce déconnecte</li>
+        <li>Sauvegarde du monde toute les 15 minutes</li>
+        <li>Niveau des dinos entre 1 et 150</li>
+        <li>Crosshair et hitmarker pour les armes à feu</li>
+        <li>
+          Commande gamma autorisé dans la console, utile pour les nuits sombres
+        </li>
+        <li>Affichage des autres joueurs sur la carte</li>
+        <li>La nuit est deux fois plus rapide que le jour.</li>
+        <li>Recolte des ressources 5x plus éfficace</li>
+        <li>
+          Votre personnage a faim moins rapidement (valeur acutel 0.6 valeur
+          original 1)
+        </li>
+        <li>
+          Votre personnage se désydrate plus lentement (valeur actuel 0.75,
+          valeur original 1)
+        </li>
+        <li>Capture des dinos 6x plus rapide</li>
+        <li>XP gagné 7x plus rapidement</li>
+        <li>
+          Toute les commandes éxécuté par une administrateur sont affiché dans
+          le chat
+        </li>
+        <li>N'importe quel joueur est autorisé à s'occupé d'un bébé dino</li>
+        <li>Les dommages effectué sous forme de texte n'ai pas affiché</li>
       </ul>
     </main>
     <footer class="page-footer">
