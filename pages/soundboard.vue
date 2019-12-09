c33004d59dfddec5ceb57321e4a3ad9da8fcb8c1
Skullmasher <skullm4sher@gmail.com>
2019-12-09 17:35:20 +0100

    :skull: bad ortografe in ARK page SEO meta tag

--- pages/soundboard.vue
+++ pages/soundboard.vue
@@ -11,7 +11,7 @@
     <main class="wrapper">
       <h1 class="txtcenter">{{ title }}</h1>
       <p>Içi un moyen de filtrer les sons par catégorie</p>
-      <button class="btn--big">lodr c skul</button>
+      <button class="btn--soundboard">lodr c skul</button>
       <p>Partager moi les fichiers audio sur le cloud plz</p>
     </main>
     <footer class="page-footer">
@@ -81,4 +81,10 @@ export default {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.btn--soundboard {
+  &:hover {
+    background-color: #126c99;
+  }
+}
+</style>
