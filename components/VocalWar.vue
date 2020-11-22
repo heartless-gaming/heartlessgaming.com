9fde24dbd8ba44fc357fef7b200ca31af853487b
Skullmasher <skullm4sher@gmail.com>
2020-11-23 00:57:50 +0100

    feat: :skull: Make Vocalwar components resposive

--- components/VocalWar.vue
+++ components/VocalWar.vue
@@ -1,46 +1,48 @@
 <template>
-  <section class="max-w-2xl mx-auto mb-10 text-gray-200">
+  <section class="max-w-4xl mx-auto mb-10 text-gray-200">
     <h2 class="mb-3 text-2xl">Discution vocal</h2>
     <p class="mb-5">
       Il y a dans cette communauté une fracture.
       <strong class="uppercase">Choisis ton camp</strong>
     </p>
-    <div class="flex mb-8">
-      <a class="mr-5" href="mumble://www.heartlessgaming.com/">
-        <svg-mumble class="w-24 voip-icons" />
-      </a>
-      <div>
-        <p class="mb-5">
-          <strong>L'expérience original Heartless</strong> ce trouve sur
-          Mumble&nbsp;! On dors dans le channel AFK et on discute même quand on
-          a pas envie de jouer.
-        </p>
-        <a
-          class="px-4 py-2 rounded-full bg-hlsred hover:bg-hlsred-dark transition-all duration-200"
-          href="mumble://www.heartlessgaming.com/"
-        >
-          <strong>Rejoindre le mumble</strong>
-          <svg-arrow class="ml-1 w-5 inline" />
+    <div class="md:flex">
+      <div class="flex mb-8">
+        <a class="mr-5" href="mumble://www.heartlessgaming.com/">
+          <svg-mumble class="w-24 voip-icons" />
         </a>
+        <div>
+          <p class="mb-5">
+            <strong>L'expérience original Heartless</strong> ce trouve sur
+            Mumble&nbsp;! On dors dans le channel AFK et on discute même quand
+            on a pas envie de jouer.
+          </p>
+          <a
+            class="px-4 py-2 rounded-full bg-hlsred hover:bg-hlsred-dark transition-all duration-200"
+            href="mumble://www.heartlessgaming.com/"
+          >
+            <strong>Rejoindre le mumble</strong>
+            <svg-arrow class="ml-1 w-5 inline" />
+          </a>
+        </div>
       </div>
-    </div>
-    <div class="flex">
-      <a class="mr-5" href="https://discord.gg/rWDzacA">
-        <svg-discord class="w-24 voip-icons" />
-      </a>
-      <div>
-        <p class="mb-5">
-          <strong>Ici c'est la modernité !</strong> Alerte et information
-          serveur automatique. Les joueurs ici sont sonvent un peu plus
-          compétitif (CSGO, LOL).
-        </p>
-        <a
-          class="px-4 py-2 rounded-full bg-hlsred hover:bg-hlsred-dark transition-all duration-200"
-          href="mumble://www.heartlessgaming.com/"
-        >
-          <strong>Rejoindre le discord</strong>
-          <svg-arrow class="ml-1 w-5 inline" />
+      <div class="flex">
+        <a class="mr-5" href="https://discord.gg/rWDzacA">
+          <svg-discord class="w-24 voip-icons" />
         </a>
+        <div>
+          <p class="mb-5">
+            <strong>Ici c'est la modernité !</strong> Alerte et information
+            serveur automatique. Les joueurs ici sont sonvent un peu plus
+            compétitif (CSGO, LOL).
+          </p>
+          <a
+            class="px-4 py-2 rounded-full bg-hlsred hover:bg-hlsred-dark transition-all duration-200"
+            href="mumble://www.heartlessgaming.com/"
+          >
+            <strong>Rejoindre le discord</strong>
+            <svg-arrow class="ml-1 w-5 inline" />
+          </a>
+        </div>
       </div>
     </div>
   </section>
