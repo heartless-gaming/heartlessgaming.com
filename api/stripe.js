e9a1728304407bff1279d645ec25e1e513e94385
Skullmasher <skullm4sher@gmail.com>
2020-12-05 03:13:41 +0100

    feat(stripe): :skull: Primitive stripe integration complete

--- api/stripe.js
+++ api/stripe.js
@@ -1,14 +1,32 @@
 import express from 'express'
+import Stripe from 'stripe'
 
 const app = express()
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
-const topkek = (req, res) => {
-  res.send('yay boi !')
+const calculateOrderAmount = (items) => {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  return 66600
 }
+
+app.post('/create-payment-intent', async (req, res) => {
+  // const { items } = req.body
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(),
+    currency: 'eur',
+  })
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  })
+})
 
 /**
  * Routes of the api prefixed with /api in nuxt.config.js
  */
-app.get('/stripe', topkek)
+// app.get('/stripe', topkek)
 
 module.exports = app
--- pages/donation.vue
+++ pages/donation.vue
@@ -17,18 +17,19 @@
     </header>
     <main>
       <div class="mb-12 max-w-2xl mx-auto">
-        <form id="payment-form" action="/charge" method="post">
-          <div class="form-row">
-            <label for="card-element">Credit or debit card</label>
-            <div id="card-element">
-              <!-- A Stripe Element will be inserted here. -->
-            </div>
-
-            <!-- Used to display form errors. -->
-            <div id="card-errors" role="alert"></div>
-          </div>
-
-          <button>Submit Payment</button>
+        <form id="payment-form">
+          <input id="email" type="text" placeholder="Email address" />
+          <div id="card-element"></div>
+          <button id="submit" class="" :disabled="isSubmitDisable">
+            <div id="spinner" class="spinner hidden"></div>
+            <span id="button-text">Donner</span>
+          </button>
+          <p id="card-error" role="alert">{{ cardErrorMsg }}</p>
+          <p class="result-message hidden">
+            Payment succeeded, see the result in your
+            <a href="" target="_blank">Stripe dashboard.</a>Refresh the page to
+            pay again.
+          </p>
         </form>
       </div>
     </main>
@@ -60,6 +61,8 @@ export default {
   },
   data: () => ({
     title: 'Donation',
+    isSubmitDisable: true,
+    cardErrorMsg: '',
   }),
   head() {
     return {
@@ -95,94 +98,279 @@ export default {
     }
   },
   mounted() {
-    const elements = this.$stripe.elements()
-    const style = {
-      base: {
-        color: '#32325d',
-        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-        fontSmoothing: 'antialiased',
-        fontSize: '16px',
-        '::placeholder': {
-          color: '#aab7c4',
-        },
+    // const elements = this.$stripe.elements()
+    // Disable the button until we have Stripe set up on the page
+    // this.isSubmitDisable = false
+    const baseURL = this.$nuxt.context.$config.baseURL
+    fetch(`${baseURL}/api/create-payment-intent`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
       },
-      invalid: {
-        color: '#fa755a',
-        iconColor: '#fa755a',
-      },
-    }
-    const card = elements.create('card', { style })
-
-    // Add an instance of the card Element into the `card-element` <div>.
-    card.mount('#card-element')
-
-    // Handle real-time validation errors from the card Element.
-    card.on('change', (event) => {
-      const displayError = document.getElementById('card-errors')
-      if (event.error) {
-        displayError.textContent = event.error.message
-      } else {
-        displayError.textContent = ''
-      }
     })
-
-    // Submit the form with the token ID.
-    const stripeTokenHandler = (token) => {
-      // Insert the token ID into the form so it gets submitted to the server
-      const form = document.getElementById('payment-form')
-      const hiddenInput = document.createElement('input')
-      hiddenInput.setAttribute('type', 'hidden')
-      hiddenInput.setAttribute('name', 'stripeToken')
-      hiddenInput.setAttribute('value', token.id)
-      form.appendChild(hiddenInput)
-
-      // Submit the form
-      form.submit()
-    }
-
-    // Handle form submission.
-    const form = document.getElementById('payment-form')
-    form.addEventListener('submit', (event) => {
-      event.preventDefault()
-
-      this.$stripe.createToken(card).then((result) => {
-        if (result.error) {
-          // Inform the user if there was an error.
-          const errorElement = document.getElementById('card-errors')
-          errorElement.textContent = result.error.message
-        } else {
-          // Send the token to your server.
-          stripeTokenHandler(result.token)
-        }
+      .then((result) => {
+        return result.json()
       })
-    })
+      .then((data) => {
+        const elements = this.$stripe.elements()
+        const style = {
+          base: {
+            color: '#32325d',
+            fontFamily: 'Arial, sans-serif',
+            fontSmoothing: 'antialiased',
+            fontSize: '16px',
+            '::placeholder': {
+              color: '#32325d',
+            },
+          },
+          invalid: {
+            fontFamily: 'Arial, sans-serif',
+            color: '#fa755a',
+            iconColor: '#fa755a',
+          },
+        }
+
+        const card = elements.create('card', { style })
+        // Stripe injects an iframe into the DOM
+        card.mount('#card-element')
+
+        card.on('change', (event) => {
+          // Disable the Pay button if there are no card details in the Element
+          this.isSubmitDisable = event.empty
+          this.cardErrorMsg = event.error ? event.error.message : ''
+        })
+
+        const form = document.getElementById('payment-form')
+        form.addEventListener('submit', (event) => {
+          event.preventDefault()
+          // Complete payment when the submit button is clicked
+          payWithCard(this.$stripe, card, data.clientSecret)
+        })
+      })
+
+    // Calls stripe.confirmCardPayment
+    // If the card requires authentication Stripe shows a pop-up modal to
+    // prompt the user to enter authentication details without leaving your page
+    const payWithCard = (stripe, card, clientSecret) => {
+      loading(true)
+      stripe
+        .confirmCardPayment(clientSecret, {
+          receipt_email: document.getElementById('email').value,
+          payment_method: { card },
+        })
+        .then((result) => {
+          if (result.error) {
+            // Show error to your customer
+            showError(result.error.message)
+          } else {
+            // The payment succeeded!
+            orderComplete(result.paymentIntent.id)
+          }
+        })
+    }
+
+    /* ------- UI helpers ------- */
+
+    // Shows a success message when the payment is complete
+    const orderComplete = (paymentIntentId) => {
+      loading(false)
+      document
+        .querySelector('.result-message a')
+        .setAttribute(
+          'href',
+          `https://dashboard.stripe.com/test/payments/${paymentIntentId}`
+        )
+      document.querySelector('.result-message').classList.remove('hidden')
+      this.isSubmitDisable = true
+    }
+
+    // Show the customer the error from Stripe if their card fails to charge
+    const showError = (errorMsgText) => {
+      loading(false)
+      const errorMsg = document.querySelector('#card-error')
+      errorMsg.textContent = errorMsgText
+      setTimeout(() => {
+        errorMsg.textContent = ''
+      }, 4000)
+    }
+
+    // Show a spinner on payment submission
+    const loading = (isLoading) => {
+      if (isLoading) {
+        // Disable the button and show a spinner
+        this.isSubmitDisable = true
+        document.querySelector('#spinner').classList.remove('hidden')
+        document.querySelector('#button-text').classList.add('hidden')
+      } else {
+        this.isSubmitDisable = false
+        document.querySelector('#spinner').classList.add('hidden')
+        document.querySelector('#button-text').classList.remove('hidden')
+      }
+    }
   },
 }
 </script>
 
-<style>
-.StripeElement {
-  box-sizing: border-box;
-  height: 40px;
-  padding: 10px 12px;
-  border: 1px solid transparent;
-  border-radius: 4px;
-  background-color: white;
-  box-shadow: 0 1px 3px 0 #e6ebf1;
-  -webkit-transition: box-shadow 150ms ease;
-  transition: box-shadow 150ms ease;
+<style scoped>
+form {
+  width: 30vw;
+  min-width: 500px;
+  align-self: center;
+  border-radius: 7px;
+  padding: 40px;
 }
 
-.StripeElement--focus {
-  box-shadow: 0 1px 3px 0 #cfd7df;
+input {
+  border-radius: 6px;
+  margin-bottom: 6px;
+  padding: 12px;
+  border: 1px solid rgba(50, 50, 93, 0.1);
+  height: 44px;
+  font-size: 16px;
+  width: 100%;
+  background: white;
 }
 
-.StripeElement--invalid {
-  border-color: #fa755a;
+.result-message {
+  line-height: 22px;
+  font-size: 16px;
 }
 
-.StripeElement--webkit-autofill {
-  background-color: #fefde5 !important;
+.result-message a {
+  color: rgb(89, 111, 214);
+  font-weight: 600;
+  text-decoration: none;
+}
+
+.hidden {
+  display: none;
+}
+
+#card-error {
+  color: rgb(105, 115, 134);
+  text-align: left;
+  font-size: 13px;
+  line-height: 17px;
+  margin-top: 12px;
+}
+
+#card-element {
+  border-radius: 4px 4px 0 0;
+  padding: 12px;
+  border: 1px solid rgba(50, 50, 93, 0.1);
+  height: 44px;
+  width: 100%;
+  background: white;
+}
+
+#payment-request-button {
+  margin-bottom: 32px;
+}
+
+/* Buttons and links */
+button {
+  background: #cc1b00;
+  color: #fff;
+  font-family: Arial, sans-serif;
+  border-radius: 0 0 4px 4px;
+  border: 0;
+  padding: 12px 16px;
+  font-size: 16px;
+  font-weight: 600;
+  cursor: pointer;
+  display: block;
+  transition: all 0.2s ease;
+  box-shadow: 0 4px 5.5px 0 rgba(0, 0, 0, 0.07);
+  width: 100%;
+}
+
+button:hover {
+  filter: contrast(115%);
+}
+button:disabled {
+  opacity: 0.5;
+  cursor: default;
+}
+
+/* spinner/processing state, errors */
+.spinner,
+.spinner::before,
+.spinner::after {
+  border-radius: 50%;
+}
+
+.spinner {
+  color: #fff;
+  font-size: 22px;
+  text-indent: -99999px;
+  margin: 0 auto;
+  position: relative;
+  width: 20px;
+  height: 20px;
+  box-shadow: inset 0 0 0 2px;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+}
+
+.spinner::before,
+.spinner::after {
+  position: absolute;
+  content: '';
+}
+
+.spinner::before {
+  width: 10.4px;
+  height: 20.4px;
+  background: #cc1b00;
+  border-radius: 20.4px 0 0 20.4px;
+  top: -0.2px;
+  left: -0.2px;
+  -webkit-transform-origin: 10.4px 10.2px;
+  transform-origin: 10.4px 10.2px;
+  -webkit-animation: loading 2s infinite ease 1.5s;
+  animation: loading 2s infinite ease 1.5s;
+}
+
+.spinner::after {
+  width: 10.4px;
+  height: 10.2px;
+  background: #cc1b00;
+  border-radius: 0 10.2px 10.2px 0;
+  top: -0.1px;
+  left: 10.2px;
+  -webkit-transform-origin: 0 10.2px;
+  transform-origin: 0 10.2px;
+  -webkit-animation: loading 2s infinite ease;
+  animation: loading 2s infinite ease;
+}
+
+@-webkit-keyframes loading {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes loading {
+  0% {
+    -webkit-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+
+@media only screen and (max-width: 600px) {
+  form {
+    width: 80vw;
+  }
 }
 
 .gray-gradient {
