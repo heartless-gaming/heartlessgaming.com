82811a47b39d7a1f7a0ffe8809692794270d2270
Skullmasher <skullm4sher@gmail.com>
2020-12-06 04:01:25 +0100

    feat(donation): :skull: Sublimate donation form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
 module.exports = {
-  future: {
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
   theme: {
     extend: {
       colors: {
         hlsred: {
           light: '#fa4000',
-          default: '#cc1b00',
+          DEFAULT: '#cc1b00',
           dark: '#be0e00',
         },
         ytred: '#d42627',
@@ -17,12 +13,23 @@ module.exports = {
       borderWidth: {
         3: '3px',
       },
+      padding: {
+        11: '2.75rem',
+      },
+      inset: {
+        0.5: '0.125rem',
+        1: '0.25rem',
+        3: '0.75rem',
+        4: '1rem',
+      },
       transitionProperty: ['motion-reduce'],
     },
-    variants: {
-      extend: {
-        opacity: ['disabled'],
-      },
+  },
+  variants: {
+    extend: {
+      opacity: ['disabled'],
+      ringWidth: ['hover'],
+      ringColor: ['hover'],
     },
   },
   plugins: [require('@tailwindcss/typography')],
