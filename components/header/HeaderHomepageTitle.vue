851f22aa41ba252a13b90fff168ab096e8911100
Skullmasher <skullm4sher@gmail.com>
2020-12-10 06:41:53 +0100

    feat(httpcode): :skull: The most basic error 404 & 500 pag

--- components/header/HeaderHomepageTitle.vue
+++ components/header/HeaderHomepageTitle.vue
@@ -1,13 +1,7 @@
 <template>
   <h1
-    class="max-w-lg mx-auto mb-10 text-gray-200 text-3xl font-grandstander zoomIn header-homepage-title-text-shadow"
+    class="max-w-lg mx-auto mb-10 text-gray-200 text-3xl font-grandstander zoomIn page-title-text-shadow"
   >
     Une communauté de gamers pour l'amour de l'entertainment&nbsp;!
   </h1>
 </template>
-
-<style lang="scss">
-.header-homepage-title-text-shadow {
-  text-shadow: #000 0 0 10px, #000 0 0 10px;
-}
-</style>
---
+++ layouts/error.vue
@@ -0,0 +1,42 @@
+<template>
+  <main class="bg-gray-900 flow-root">
+    <div class="max-w-lg mx-auto my-24 sm:my-48 text-center text-gray-200">
+      <h2 v-if="statusCode === 404" class="text-6xl font-bold mb-5">404</h2>
+      <p v-else class="text-6xl font-bold mb-5">500</p>
+      <NuxtLink
+        to="/"
+        class="inline-block bg-hlsred py-3 px-6 rounded hover:bg-hlsred-dark transition-all duration-200 uppercase shadow-lg"
+      >
+        Retour à la page d'accueil
+      </NuxtLink>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'NuxtError',
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+  head() {
+    return {
+      title: 'Erreur',
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width,initial-scale=1.0,minimum-scale=1.0',
+        },
+      ],
+    }
+  },
+  computed: {
+    statusCode() {
+      return (this.error && this.error.statusCode) || 500
+    },
+  },
+}
+</script>
