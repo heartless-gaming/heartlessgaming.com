b0a5c35fc32760f8432e680b04c3545a8e7e58bc
Skullmasher <skullm4sher@gmail.com>
2020-12-12 14:25:07 +0100

    feat(privacy): :skull: Donation privacy notice

--- layouts/error.vue
+++ layouts/error.vue
@@ -4,11 +4,19 @@
       <h2 v-if="statusCode === 404" class="text-6xl font-bold mb-5">404</h2>
       <p v-else class="text-6xl font-bold mb-5">500</p>
       <NuxtLink
+        v-if="statusCode === 404"
         to="/"
         class="inline-block bg-hlsred py-3 px-6 rounded hover:bg-hlsred-dark transition-all duration-200 uppercase shadow-lg"
       >
         Retour à la page d'accueil
       </NuxtLink>
+      <button
+        v-else
+        :click="window.location.reload(true)"
+        class="inline-block bg-hlsred py-3 px-6 rounded hover:bg-hlsred-dark transition-all duration-200 uppercase shadow-lg"
+      >
+        Recharger la page (spam F5!!!)
+      </button>
     </div>
   </main>
 </template>
