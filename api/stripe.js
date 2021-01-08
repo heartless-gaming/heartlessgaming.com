b29becc0d21096dd6e35d81e1c47d98f52689b66
Skullmasher <SkullMasher@users.noreply.github.com>
2021-01-08 01:09:20 +0100

    feat(api): :skull: Fetch stripe payouts & ovh bills endpoint

--- api/stripe.js
+++ api/stripe.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import Stripe from 'stripe'
 import bodyParser from 'body-parser'
+import Redis from 'ioredis'
+import Stripe from 'stripe'
 
 const app = express()
 app.use(bodyParser.json())
+const redis = new Redis()
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
@@ -14,7 +16,7 @@ const calculateOrderAmount = (amount) => {
   return amount * 100
 }
 
-app.post('/create-payment-intent', async (req, res) => {
+const createPaymentIntent = async (req, res) => {
   const amount = req.body.amount
   // Create a PaymentIntent with the order amount and currency
   const paymentIntent = await stripe.paymentIntents.create({
@@ -25,11 +27,43 @@ app.post('/create-payment-intent', async (req, res) => {
   res.send({
     clientSecret: paymentIntent.client_secret,
   })
-})
+}
 
-/**
- * Routes of the api prefixed with /api in nuxt.config.js
- */
-// app.get('/stripe', topkek)
+const stripePayouts = async (req, res) => {
+  const payouts = await stripe.payouts.list({
+    // limit: 3,
+  })
+
+  const response = payouts.data.reduce((acc, payout) => {
+    const obj = {
+      date: payout.created,
+      description: 'Donation',
+      amount: payout.amount,
+    }
+
+    acc.push(obj)
+
+    return acc
+  }, [])
+
+  // Send the data to redis with an expiration value of 6 hours
+  redis.setex('heartlesspayoutstripe', 21600, JSON.stringify(response))
+
+  res.json(response)
+}
+
+// Stripe payout cache middleware with redis key 'heartlesspayoutstripe'
+const stripePayoutsCache = async (req, res, next) => {
+  const heartlesspayoutstripe = await redis.get('heartlesspayoutstripe')
+
+  if (heartlesspayoutstripe === null) {
+    next()
+  } else {
+    res.json(JSON.parse(heartlesspayoutstripe))
+  }
+}
+
+app.post('/create-payment-intent', createPaymentIntent)
+app.get('/stripePayouts', stripePayoutsCache, stripePayouts)
 
 module.exports = app
