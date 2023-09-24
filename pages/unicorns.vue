0bcc5d98cb125a86231aba294776c22310ac89dc
Skullmasher <4488386+SkullMasher@users.noreply.github.com>
2023-09-24 09:42:57 +0200

    feat: create a unicorn easter egg page

---
+++ pages/unicorns.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="grid items-center justify-center h-screen gradient">
+    <h1 class="animate-bounce text-7xl text-white">
+      🦄UNICORNS🦄
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+  .gradient {
+    background: linear-gradient(135deg, rgba(16,162,142,1) 0%, rgba(121,9,101,1) 35%, rgba(115,18,108,1) 57%, rgba(0,212,255,1) 100%);
+  }
+</style>
