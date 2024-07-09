fa4b52f0a7bc2ef2e2ef2cd29b80a833933e2557
Skullmasher <4488386+SkullMasher@users.noreply.github.com>
2024-07-10 01:05:48 +0200

    chore: Let's restart again fresh nuxt install

--- components/TempLink.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-  defineProps({
-    url: String
-  })
-</script>
-
-<template>
-  <a class="text-blue-500 hover:text-blue-700 transition" :href="url"><slot>fallback link Text</slot></a>
-</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: [
-    '@unocss/nuxt',
-  ],
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true }
 })
