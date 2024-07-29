0b22d4a058b5aeac22d501ae28ce35e3c5297a79
Skullmasher <4488386+SkullMasher@users.noreply.github.com>
2024-07-29 23:56:28 +0200

    feat: ts quote

---
+++ app/components/FooterIconLinks.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="mb-8 grid grid-cols-3 sm:grid-cols-5 gap-y-6 gap-x-2">
+                <div class="tooltip" data-tip="Youtube">
+                    <a href="https://www.youtube.com/@heartless.gaming/videos" class="btn btn-ghost">
+                        <Icon name="logos:youtube-icon" class="text-4xl" />
+                    </a>
+                </div>
+                <div class="tooltip" data-tip="Twitch">
+                    <a href="https://www.twitch.tv/heartlessgaming_live" class="btn btn-ghost">
+                        <Icon name="logos:twitch" class="text-4xl"/>
+                    </a>
+                </div>
+                <div class="tooltip" data-tip="Mumble">
+                    <a href="mumble://heartlessgaming.com" class="btn btn-ghost">
+                        <Icon name="simple-icons:mumble" class="text-4xl"/>
+                    </a>
+                </div>
+                <div class="tooltip" data-tip="Discord">
+                    <a href="https://discord.gg/rWDzacA" class="btn btn-ghost">
+                        <Icon name="logos:discord-icon" class="text-4xl"/>
+                    </a>
+                </div>
+                <div class="tooltip" data-tip="Github">
+                    <a href="https://github.com/heartless-gaming/heartlessgaming.com" class="btn btn-ghost">
+                        <Icon name="mdi:github" class="text-4xl"/>
+                    </a>
+                </div>
+            </div>
+</template>
