0ab6d0270078fa0e65e94be76265d5bd4db17309
Skullmasher <4488386+SkullMasher@users.noreply.github.com>
2024-08-29 18:19:58 +0200

    stripe payment intent

---
+++ server/api/create-payment-intent.ts
@@ -0,0 +1,29 @@
+import Stripe from 'stripe'
+
+export default defineEventHandler(async (event) => {
+  const { stripeSecretKey } = useRuntimeConfig(event)
+
+  const stripe = new Stripe(stripeSecretKey)
+
+  const orderAmount = 2700
+  let paymentIntent
+
+  try {
+    paymentIntent = await stripe.paymentIntents.create({
+      currency: 'eur',
+      amount: orderAmount,
+      automatic_payment_methods: { enabled: true },
+    })
+
+    // Send publishable key and PaymentIntent details to client
+    return {
+      clientSecret: paymentIntent.client_secret,
+    }
+  }
+  catch (e) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: e.message,
+    })
+  }
+})
