438f4de9d6254ba6877d5f624356e27e0e3668fa
Skullmasher <4488386+SkullMasher@users.noreply.github.com>
2024-09-28 23:53:01 +0200

    test theme swither

---
+++ app/components/ThemeDropdown.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="dropdown mb-72">
+    <div tabindex="0" role="button" class="btn m-1">
+      <Icon
+        name="mdi:palette-swatch-variant"
+        class="text-xl"
+      />
+      <span>Themes</span>
+    </div>
+    <ul tabindex="0" class="dropdown-content bg-base-300 rounded-box z-[1] w-52 p-2 shadow-2xl">
+      <li>
+        <input
+          type="radio"
+          name="theme-dropdown"
+          class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+          aria-label="Default"
+          value="default"
+        >
+      </li>
+      <li>
+        <input
+          type="radio"
+          name="theme-dropdown"
+          class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+          aria-label="Retro"
+          value="retro"
+        >
+      </li>
+      <li>
+        <input
+          type="radio"
+          name="theme-dropdown"
+          class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+          aria-label="Cyberpunk"
+          value="cyberpunk"
+        >
+      </li>
+      <li>
+        <input
+          type="radio"
+          name="theme-dropdown"
+          class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+          aria-label="Valentine"
+          value="valentine"
+        >
+      </li>
+      <li>
+        <input
+          type="radio"
+          name="theme-dropdown"
+          class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+          aria-label="Aqua"
+          value="aqua"
+        >
+      </li>
+    </ul>
+  </div>
+</template>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,51 @@
 import type { Config } from 'tailwindcss'
-import daisyui from "daisyui"
+import daisyui from 'daisyui'
 
 export default <Partial<Config>>{
-    theme: {
-        extend: {
-            dropShadow: {
-                'logo': '0 10px 10px rgba(0, 0, 0, 1)',
-            }
-        }
+  theme: {
+    extend: {
+      dropShadow: {
+        logo: '0 10px 10px rgba(0, 0, 0, 1)',
+      },
     },
-    plugins: [
-        daisyui,
+  },
+  plugins: [
+    daisyui,
+  ],
+  daisyui: {
+    themes: [
+      'light',
+      'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter',
+      'dim',
+      'nord',
+      'sunset',
     ],
+  },
 }
