d737ad96aea877075d7401294f949a62d60a2d69
Skullmasher <4488386+SkullMasher@users.noreply.github.com>
2024-10-05 03:30:17 +0200

    fix app.vue & add favicon & L as default size & shirt info as callapsible

--- app/components/Shirt.vue
+++ app/components/Shirt.vue
@@ -21,7 +21,7 @@ const colors = [
 ]
 
 const activeColor = ref(2)
-const activeSize: Ref<null | number> = ref(null)
+const activeSize: Ref<null | number> = ref(2)
 
 const activeImagePath = computed(() => `/img/shirt/embroidered-heartlessgaming-t-shirt-${toKebab(colors[activeColor.value].name)}.jpg`)
 
@@ -113,52 +113,53 @@ async function buy() {
         {{ size }}
       </button>
     </div>
-    <h3 class="text-8xl font-bold flex items-center gap-x-2 mb-6">
-      <span class="tracking-wide">27</span>
-      <Icon name="ic:baseline-euro" />
+    <h3 class="text-9xl font-bold flex items-end gap-x-2">
+      <span class="tracking-wide">27 €</span>
     </h3>
-    <div class="mb-6">
-      <pre>topkek: {{ activeSize }}</pre>
-      <p v-show="!Number.isFinite(activeSize)" class="text-warning">
-        Veuilez choisir une taille pour acheter
-      </p>
+    <p class="text-xl mb-6">
+      TTC & frais de livraison inclut
+    </p>
+    <div class="mb-16">
       <button
         class="btn btn-accent uppercase btn-lg"
-        :disabled="!Number.isFinite(activeSize)"
         @click="buy"
       >
         Acheter
       </button>
     </div>
-    <button class="btn btn-secondary" @click="buy">
-      BUY !!
-    </button>
     <div ref="paymentEl" />
-    <div class="leading-relaxed">
-      <p class="font-bold">
-        Information suplémentaires :
-      </p>
-      <ul class="list-inside pl-4">
-        <li>
-          Broderie sur un <NuxtLink
-            class="pretty-link"
-            to="https://stanleystella.com/fr-fr/creator-2-0-sttu169"
-          >
-            T-shirt Stanley/Stella 100% coton biologique vegan
-          </NuxtLink>
-        </li>
-        <li>
-          Imprimé à la demande (dropshiping) par <NuxtLink
-            to="https://www.printful.com"
-            class="pretty-link"
-          >
-            Printful
-          </NuxtLink>
-        </li>
-        <li>Cout de fabrication du tee shirt entre 13,95€ & 19,15€</li>
-        <li>Prix d'expédition entre 3.99€ & 10.59€</li>
-        <li>Marge entre 7.85€ & 13.05€</li>
-      </ul>
+    <div class="bg-base-200 collapse collapse-arrow">
+      <input type="checkbox" checked="true" class="peer">
+      <div
+        class="collapse-title bg-base-200"
+      >
+        Information suplémentaires
+      </div>
+      <div
+        class="collapse-content bg-base-200"
+      >
+        <ul class="list-inside pl-4 list-disc">
+          <li>
+            Broderie sur un <NuxtLink
+              class="pretty-link"
+              to="https://stanleystella.com/fr-fr/creator-2-0-sttu169"
+            >
+              T-shirt Stanley/Stella 100% coton biologique vegan
+            </NuxtLink>
+          </li>
+          <li>
+            Imprimé à la demande (dropshiping) par <NuxtLink
+              to="https://www.printful.com"
+              class="pretty-link"
+            >
+              Printful
+            </NuxtLink>
+          </li>
+          <li>Cout de fabrication du tee shirt 13,95€, 19,15€ pour les tailles à partie de 3XL</li>
+          <li>Prix d'expédition entre 3.99€ & 10.59€, 5€ vers la France</li>
+          <li>Marge maximal sur chaque vente: 6€</li>
+        </ul>
+      </div>
     </div>
   </section>
 </template>
