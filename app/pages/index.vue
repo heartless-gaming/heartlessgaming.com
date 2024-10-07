e353edee7f968d98d6500d1b27e3f87a008d06ce
Skullmasher <4488386+SkullMasher@users.noreply.github.com>
2024-10-07 12:05:37 +0200

    hero effect

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
     <Navbar />
-    <header class="text-center group mb-16">
+    <header class="text-center group mb-16 relative">
+      <HeroEffect />
       <div class="max-w-xl mx-auto px-2">
         <SvgLogoHeartlessgamingDark2020
           class="box-content max-w-xs mx-auto px-8 pb-5 drop-shadow-logo group-hover:-rotate-2 group-hover:scale-105 transition-transform duration-1000"
           :font-controlled="false"
         />
-        <h1 class="text-3xl uppercase font-bold">
+        <h1 class="text-3xl uppercase font-bold" style="text-shadow:rgba(0, 0, 0, 0.5) 0px 5px 10px, #000 0px 5px 20px;">
           Communauté <abbr title="Groupe de joueur partageant plusieurs jeux en ligne.">Multigaming</abbr>
         </h1>
       </div>
