b0bf205932a75b67d34879ee789877fc39cf4806
Skullmasher <4488386+SkullMasher@users.noreply.github.com>
2025-07-25 16:55:40 +0200

    fix: attempt 3 to fix nuxt security hurdle

--- app/stores/useCartStore.ts
+++ app/stores/useCartStore.ts
@@ -1,8 +1,8 @@
 export const useCartStore = defineStore('cart', () => {
   const items = ref([
-    // { available: true, variant_id: 15114, name: 'Original Skull', variantName: 'Original Skull / Black / S', size: 'S', price: '29.95', color: 'Black', hex: '#282828', sku: '687588552F037_Black-S' },
-    // { available: true, variant_id: 15123, name: 'Original Skull', variantName: 'Original Skull / Red / XL', size: 'XL', price: '27.75', color: 'Red', hex: '#ba2142', sku: '687588552F037_Red-XL' },
-    // { available: true, variant_id: 15123, name: 'Original Skull', variantName: 'Original Skull / Red / XL', size: 'XL', price: '27.75', color: 'Red', hex: '#ba2142', sku: '687588552F037_Red-XL' },
+    { available: true, variant_id: 15114, name: 'Original Skull', variantName: 'Original Skull / Black / S', size: 'S', price: '29.95', color: 'Black', hex: '#282828', sku: '687588552F037_Black-S' },
+    { available: true, variant_id: 15123, name: 'Original Skull', variantName: 'Original Skull / Red / XL', size: 'XL', price: '27.75', color: 'Red', hex: '#ba2142', sku: '687588552F037_Red-XL' },
+    { available: true, variant_id: 15123, name: 'Original Skull', variantName: 'Original Skull / Red / XL', size: 'XL', price: '27.75', color: 'Red', hex: '#ba2142', sku: '687588552F037_Red-XL' },
   ])
 
   const isCartEmpty = computed(() => !items.value.length)
